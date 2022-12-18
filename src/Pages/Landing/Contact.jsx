c933571a910659162d844d9333ab2563c3cc6ca2
sikander-afzaal <sikander.aaa@gmail.com>
2022-12-18 19:12:45 +0500

    asdasd

--- src/Pages/Landing/Contact.jsx
+++ src/Pages/Landing/Contact.jsx
@@ -1,10 +1,16 @@
 import HeadingComp from "../../Components/HeadingComp";
+import Button from "../../Components/Button";
 
 const Contact = () => {
   return (
     <div id="contact" className="full mt-[80px] xl:mt-[140px] px-5">
       <HeadingComp sub="Contact" head="Get in Touch" />
-      <form className="bg-[rgba(255,255,255,0.06)] gap-[20px] mt-[30px] rounded-[24px] flex justify-start items-start flex-col w-full max-w-[704px] p-5 sm:p-7">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+        className="bg-[rgba(255,255,255,0.06)] gap-[20px] mt-[30px] rounded-[24px] flex justify-start items-start flex-col w-full max-w-[704px] p-5 sm:p-7"
+      >
         <div className="w-full justify-start items-start flex gap-[20px] sm:gap-3 sm:flex-row flex-col">
           <div className="flex justify-start items-start flex-col w-full gap-2">
             <label htmlFor="first" className="text-white text-sm font-normal">
@@ -63,6 +69,10 @@ const Contact = () => {
             className="bg-[rgba(255,255,255,0.04)] resize-none h-[120px] w-full rounded text-[rgba(255,255,255,0.48)] text-sm font-normal pt-2 border-none outline-none pl-2"
           />
         </div>
+        <Button
+          name="GET STARTED NOW"
+          classes={"bg-blue border-none font-medium text-lg w-full mt-2"}
+        />
       </form>
     </div>
   );
