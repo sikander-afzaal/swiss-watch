33cc8ca577bf321173f9dd213f883c737f8acaab
sikander-afzaal <sikander.aaa@gmail.com>
2022-12-21 21:31:40 +0500

    asdasd

--- src/Pages/Landing/Soon.jsx
+++ src/Pages/Landing/Soon.jsx
@@ -8,74 +8,151 @@ const Soon = () => {
   const watch1 = useRef();
   const watch2 = useRef();
   const watch3 = useRef();
+  const watch4 = useRef();
+  const watch5 = useRef();
   const page1 = useRef();
   const page2 = useRef();
   const page3 = useRef();
+  const page4 = useRef();
+  const page5 = useRef();
   useEffect(() => {
     gsap.registerPlugin(Flip);
-    const watchState = Flip.getState([
+    const watchArray = [
       watch1.current,
       watch2.current,
       watch3.current,
-    ]);
+      watch4.current,
+      watch5.current,
+    ];
+    const watchArrayMob = [watch1.current, watch2.current, watch3.current];
+    const watchState =
+      window.innerWidth > 1280
+        ? Flip.getState(watchArray)
+        : Flip.getState(watchArrayMob);
+    if (window.innerWidth > 1280) {
+      //removing all the old classes --------------------
+      const filteredArray__After__Old__Active__State = watchArray.filter(
+        (elem) => !elem.classList.contains("activeWatch")
+      );
+      filteredArray__After__Old__Active__State.forEach((elem, idx) => {
+        elem.classList.remove("w-[206px]");
+        elem.classList.remove("left-[0%]");
+        elem.classList.remove("-z-[20]");
+        elem.classList.remove("-z-[10]");
+        elem.classList.remove("left-[10%]");
+        elem.classList.remove("right-[10%]");
+        elem.classList.remove("right-[0%]");
+      });
 
-    [watch1.current, watch2.current, watch3.current].forEach((elem, idx) => {
-      if (idx === activeWatch - 1) {
-        elem.classList.add("watchActive");
-        elem.classList.remove("order-1");
-        elem.classList.remove("xl:max-w-[210px]");
-        elem.classList.remove("max-w-[115px]");
-        elem.classList.remove("order-2");
-        elem.classList.remove("order-3");
-      } else {
-        elem.classList.remove("watchActive");
-        elem.classList.add("xl:max-w-[210px]");
-        elem.classList.add("max-w-[115px]");
-        elem.classList.remove("order-1");
-        elem.classList.remove("order-2");
-        elem.classList.remove("order-3");
-        if (activeWatch === 1) {
-          watch3.current.classList.add("order-3");
-          watch2.current.classList.add("order-1");
-        } else if (activeWatch === 2) {
-          watch1.current.classList.add("order-1");
-          watch3.current.classList.add("order-3");
-        } else if (activeWatch === 3) {
-          watch1.current.classList.add("order-1");
-          watch2.current.classList.add("order-3");
+      watchArray.forEach((elem) => {
+        elem.classList.remove("activeWatch");
+      });
+      // ------------------------------------------
+      // adding new classes ----------------------------------
+      watchArray[activeWatch - 1].classList.add("activeWatch");
+      const filteredArray__After__New__Active__State = watchArray.filter(
+        (elem) => !elem.classList.contains("activeWatch")
+      );
+      filteredArray__After__New__Active__State.forEach((elem, idx) => {
+        elem.classList.add("w-[206px]");
+        if (idx === 0) {
+          elem.classList.add("left-[0%]");
+          elem.classList.add("-z-[20]");
+        } else if (idx === 1) {
+          elem.classList.add("left-[10%]");
+          elem.classList.add("-z-[10]");
+        } else if (idx === 2) {
+          elem.classList.add("right-[10%]");
+          elem.classList.add("-z-[10]");
+        } else if (idx === 3) {
+          elem.classList.add("right-[0%]");
+          elem.classList.add("-z-[20]");
         }
-      }
-    });
+      });
+    } else {
+      //removing all the old classes --------------------
+      const filteredArray__After__Old__Active__StateMob = watchArrayMob.filter(
+        (elem) => !elem.classList.contains("activeWatchMob")
+      );
+      filteredArray__After__Old__Active__StateMob.forEach((elem, idx) => {
+        elem.classList.remove("w-[100px]");
+        elem.classList.remove("left-[10%]");
+        elem.classList.remove("-z-[10]");
+        elem.classList.remove("right-[10%]");
+      });
+      watchArrayMob.forEach((elem) => {
+        elem.classList.remove("activeWatchMob");
+      });
+      watchArrayMob[activeWatch - 1].classList.add("activeWatchMob");
+      const filteredArray__After__New__Active__StateMob = watchArrayMob.filter(
+        (elem) => !elem.classList.contains("activeWatchMob")
+      );
+      filteredArray__After__New__Active__StateMob.forEach((elem, idx) => {
+        elem.classList.add("w-[100px]");
+        if (idx === 0) {
+          elem.classList.add("left-[10%]");
+          elem.classList.add("-z-[10]");
+        } else if (idx === 1) {
+          elem.classList.add("right-[10%]");
+          elem.classList.add("-z-[10]");
+        }
+      });
+      console.log(filteredArray__After__New__Active__StateMob);
+    }
     Flip.from(watchState, {
       duration: 2,
-      absolute: true,
       ease: Power3.easeInOut,
+      absolute: true,
     });
   }, [activeWatch]);
-  useEffect(() => {
-    const interval = setTimeout(() => {
-      //autoplay functionality --------------
-      const currentNum = activeWatch;
+  // useEffect(() => {
+  //   const interval = setTimeout(() => {
+  //     //autoplay functionality --------------
+  //     const currentNum = activeWatch;
 
-      switch (currentNum) {
-        case 1:
-          setActiveWatch(2);
-          break;
-        case 2:
-          setActiveWatch(3);
-          break;
-        case 3:
-          setActiveWatch(1);
-          break;
-        default:
-          setActiveWatch(1);
-          break;
-      }
-    }, 5000);
-    return () => {
-      clearTimeout(interval);
-    };
-  }, [activeWatch]);
+  //     if (window.innerWidth > 1280) {
+  //       switch (currentNum) {
+  //         case 1:
+  //           setActiveWatch(2);
+  //           break;
+  //         case 2:
+  //           setActiveWatch(3);
+  //           break;
+  //         case 3:
+  //           setActiveWatch(4);
+  //           break;
+  //         case 4:
+  //           setActiveWatch(5);
+  //           break;
+  //         case 5:
+  //           setActiveWatch(1);
+  //           break;
+  //         default:
+  //           setActiveWatch(1);
+  //           break;
+  //       }
+  //     } else {
+  //       switch (currentNum) {
+  //         case 1:
+  //           setActiveWatch(2);
+  //           break;
+  //         case 2:
+  //           setActiveWatch(3);
+  //           break;
+  //         case 3:
+  //           setActiveWatch(1);
+  //           break;
+
+  //         default:
+  //           setActiveWatch(1);
+  //           break;
+  //       }
+  //     }
+  //   }, 5000);
+  //   return () => {
+  //     clearTimeout(interval);
+  //   };
+  // }, [activeWatch]);
 
   return (
     <div className="full mt-[70px] relative  xl:mt-[150px] bg-[rgba(255,255,255,0.06)] ">
@@ -87,19 +164,19 @@ const Soon = () => {
             Coming Soon
           </h2>
           {window.innerWidth < 1280 ? (
-            <div className="flex  xl:min-h-[520px] min-h-[320px] justify-center xl:mt-0 mt-[50px] items-center w-full ">
+            <div className=" relative max-w-[350px]   min-h-[250px] justify-center  mt-[20px] w-full ">
               <img
                 ref={watch1}
                 onClick={() => setActiveWatch(1)}
                 src="/soon1.png"
-                className={`cursor-pointer  w-full  object-contain row-start-1 row-end-2`}
+                className={`cursor-pointer top-1/2 -translate-y-1/2  absolute  object-contain `}
                 alt=""
               />
               <img
                 ref={watch2}
                 onClick={() => setActiveWatch(2)}
                 src="/soon2.png"
-                className={`cursor-pointer w-full  object-contain row-start-1 row-end-2 `}
+                className={`cursor-pointer top-1/2 -translate-y-1/2 absolute object-contain  `}
                 alt=""
               />
               <img
@@ -107,7 +184,7 @@ const Soon = () => {
                 onClick={() => setActiveWatch(3)}
                 src="/soon3.png"
                 alt=""
-                className={`cursor-pointer  w-full  object-contain row-start-1 row-end-2`}
+                className={`cursor-pointer top-1/2 -translate-y-1/2 absolute  object-contain `}
               />
             </div>
           ) : (
@@ -142,22 +219,36 @@ const Soon = () => {
                 activeWatch === 3 ? "bg-blue" : "bg-white opacity-[0.36]"
               } transition-all`}
             ></div>
+            <div
+              onClick={() => setActiveWatch(4)}
+              ref={page4}
+              className={`w-[48px] cursor-pointer h-[6px] ${
+                activeWatch === 4 ? "bg-blue" : "bg-white opacity-[0.36]"
+              } transition-all`}
+            ></div>
+            <div
+              onClick={() => setActiveWatch(5)}
+              ref={page5}
+              className={`w-[48px] cursor-pointer h-[6px] ${
+                activeWatch === 5 ? "bg-blue" : "bg-white opacity-[0.36]"
+              } transition-all`}
+            ></div>
           </div>
         </div>
         {window.innerWidth >= 1280 ? (
-          <div className="flex  xl:min-h-[520px] min-h-[320px] justify-center xl:mt-0 mt-[50px] items-center w-full ">
+          <div className="flex max-w-[550px]     isolate relative  min-h-[450px] justify-center  items-center w-full ">
             <img
               ref={watch1}
               onClick={() => setActiveWatch(1)}
               src="/soon1.png"
-              className={`cursor-pointer  w-full  object-contain row-start-1 row-end-2`}
+              className={`cursor-pointer    top-1/2 -translate-y-1/2 absolute   object-contain `}
               alt=""
             />
             <img
               ref={watch2}
               onClick={() => setActiveWatch(2)}
               src="/soon2.png"
-              className={`cursor-pointer w-full  object-contain row-start-1 row-end-2 `}
+              className={`cursor-pointer   top-1/2 -translate-y-1/2 absolute   object-contain  `}
               alt=""
             />
             <img
@@ -165,7 +256,21 @@ const Soon = () => {
               onClick={() => setActiveWatch(3)}
               src="/soon3.png"
               alt=""
-              className={`cursor-pointer  w-full  object-contain row-start-1 row-end-2`}
+              className={`cursor-pointer absolute    top-1/2 -translate-y-1/2   object-contain `}
+            />
+            <img
+              ref={watch4}
+              onClick={() => setActiveWatch(4)}
+              src="/soon3.png"
+              alt=""
+              className={`cursor-pointer    top-1/2 -translate-y-1/2 absolute   object-contain `}
+            />
+            <img
+              ref={watch5}
+              onClick={() => setActiveWatch(5)}
+              src="/soon3.png"
+              alt=""
+              className={`cursor-pointer    top-1/2 -translate-y-1/2 object-contain absolute  `}
             />
           </div>
         ) : (
